04b04bebdfc48be67a76b83c41ff20aa3b71b820
VentsislavR <33418350+VentsislavR@users.noreply.github.com>
2018-12-03 15:07:20 +0100

    Add files via upload

---
+++ Fourth_Handin/ApiTokens.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieDB
+{
+	public class ApiTokens
+	{
+		public static string omdb = "670e75a5";
+	}
+}
---
+++ Fourth_Handin/TransformXslt.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Xsl;
+using System.IO;
+
+namespace MovieDB
+{
+	public class TransformXslt
+	{
+		private string XmlFile { get; set; }
+		private string XsltFile { get; set; }
+		private string Destination { get; set; }
+
+		public TransformXslt(string xml, string xslt, string destination)
+		{
+			XmlFile = xml;
+			XsltFile = xslt;
+			Destination = destination;
+		}
+
+		public void TransformXml()
+		{
+			//xml
+			FileStream fs = new FileStream(Destination, FileMode.Create);
+			XslCompiledTransform xct = new XslCompiledTransform();
+			xct.Load(XsltFile);
+			//xml
+			xct.Transform(XmlFile, null, fs);
+			fs.Close();
+		}
+	}
+}
