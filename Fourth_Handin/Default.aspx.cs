ea732905a3343a9df7f47ffe584df61bb3dbdb44
VentsislavR <33418350+VentsislavR@users.noreply.github.com>
2018-12-03 15:15:55 +0100

    Add files via upload

---
+++ Fourth_Handin/Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Xsl;
+using System.Data;
+using System.Net;
+
+namespace MovieDB
+{
+	public partial class Default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			TransformUsingXslt();
+		}
+
+		#region XSLT Transform
+		public void TransformUsingXslt()
+		{
+			string sourcefile = HttpContext.Current.Server.MapPath("xml/Project4.xml");
+			string xsltfile = HttpContext.Current.Server.MapPath("xslt/commercial.xslt");
+			string destination = HttpContext.Current.Server.MapPath("xml/commercials.xml");
+
+			TransformXslt transform = new TransformXslt(sourcefile, xsltfile, destination);
+			transform.TransformXml();
+		}
+		#endregion
+	}
+}
